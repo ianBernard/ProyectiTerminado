4fab1ad3667e79e8dab036b9a49291c681075050
ianBernard <82122990+ianBernard@users.noreply.github.com>
2022-11-23 08:51:23 -0300

    Add files via upload

---
+++ proyectoFinal/proyectoFinal/asdasd/asdasd/src/routes/products.routes.js
@@ -0,0 +1,23 @@
+import {Router} from 'express'
+import { getUsuarios, createUsuarios, getUsuariosById, deleteUsuarioById, getRecetas, createRecetas, getRecetasById, deleteRecetasById, getRutinas, createRutinas, getRutinasById,deleteRutinasById, LikeRecetas,LikeRutina} from '../controllers/products.controllers'
+const router = Router()
+router.get('/usuarios', getUsuarios)
+router.post('/usuarios',createUsuarios)
+router.get('/usuarios/:idUsuario', getUsuariosById)
+router.delete('/usuarios/:idUsuario', deleteUsuarioById)
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+router.get('/recetas', getRecetas)
+router.post('/recetas',createRecetas)
+router.get('/recetas/:idRecetas', getRecetasById)
+router.delete('/recetas/:idRecetas', deleteRecetasById)
+router.put('/recetas/:idRecetas',LikeRecetas)
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+router.get('/rutinas',getRutinas)
+router.post('/rutinas',createRutinas)
+router.get('/rutinas/:idRutina', getRutinasById)
+router.delete('/rutinas/:idRutina', deleteRutinasById)
+router.put('/rutinas/:idRutina',LikeRutina)
+
+
+export default router
